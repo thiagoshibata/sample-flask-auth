ed021e69931a91f59164d6ca74bfbee75d55b3ff
thiago-shibata <thiago.shibatta@gmail.com>
2024-11-14 10:34:59 -0300

    create model User & configuration database

---
+++ models/user.py
@@ -0,0 +1,12 @@
+# importando a variável db do app.py
+from database import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+  # id (int)
+  # username(text)
+  # password(text)
+
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), nullable=False, unique=True)
+  password = db.Column(db.String(80), nullable=False)
